b60f4dccb181d3bae5c9140fb362da86148fadc9
youngeun <summer_97@naver.com>
2021-04-20 17:23:43 +0900

    기초 세팅

---
+++ Front_End/altari_project_01/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ConnectedRouter } from "connected-react-router";
+import { Route } from "react-router-dom";
+import { history } from "../redux/configStore";
+import styled from 'styled-components';
+
+function App() {
+ 
+  return (
+    <React.Fragment>
+     
+    </React.Fragment>
+  );
+}
+export default App;

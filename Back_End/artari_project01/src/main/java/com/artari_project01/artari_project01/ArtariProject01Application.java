2d1c26286a52ee0124018399fb40ac1445966609
[strong1133] <[strong1133@naver.com]>
2021-04-20 17:00:10 +0900

    backendbuild

---
+++ Back_End/artari_project01/src/main/java/com/artari_project01/artari_project01/ArtariProject01Application.java
@@ -0,0 +1,13 @@
+package com.artari_project01.artari_project01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtariProject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtariProject01Application.class, args);
+    }
+
+}
---
+++ Back_End/artari_project01/src/test/java/com/artari_project01/artari_project01/ArtariProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artari_project01.artari_project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtariProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
